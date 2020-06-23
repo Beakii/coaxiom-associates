cb661b191be49f42360a8bbe06dad1bac8c3de1c
Beakii <auminist3@gmail.com>
2020-06-23 15:32:06 +1200

    changed navbar styling and layout, added anchorpoint offsets for navscrolling

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -2,6 +2,7 @@ const navActive = () => {
     const burger = document.querySelector('.hamburger');
     const nav = document.querySelector('.nav-links')
     const navLinks = document.querySelectorAll('.nav-links li')
+    const landing = document.querySelector('.inactive-page');
 
     
     burger.addEventListener('click', ()=>{
@@ -19,11 +20,28 @@ const navActive = () => {
             }
         });
 
+        //add background black
+        landing.classList.toggle('landing-dark');
+
         //toggle hamburger animation
         burger.classList.toggle('toggle');
     });
 
+    //disable nav if click on the black box surrounding it
+    landing.addEventListener('click', () =>{
+        nav.classList.remove('nav-active');
+        burger.classList.remove('toggle');
+        landing.classList.remove('landing-dark');
+    });
 
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 500) {
+           $('.navbar').addClass('navbar-dark');
+        } else {
+           $('.navbar').removeClass('navbar-dark');
+        }
+    });
 }
 
 navActive();
